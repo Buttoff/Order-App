31f2c6fc13a35984a8bf608d2e71b70400d55af5
3457862348975 <shevlyagin2008@mail.ru>
2025-05-29 12:18:19 +0300

    Update

---
+++ WindowsFormsApp2/OrderSummary.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderSummaryApp
+{
+    public class OrderSummary
+    {
+        public string ClientNumber { get; set; }
+        public string ShippingAddress { get; set; }
+        public string DeliveryAddress { get; set; }
+        public int PackingType { get; set; }
+        public string TransportType { get; set; }
+    }
+}
